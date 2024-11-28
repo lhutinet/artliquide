5acc001c5d7b3a6f4f63719661d52faddddd709e
laurent <lhutinet@free.fr>
2024-11-28 16:55:58 +0100

    Add files via upload

---
+++ script/navb.js
@@ -0,0 +1,8 @@
+fetch("nav.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch((error) =>
+    console.error("Erreur lors du chargement de la navigation :", error)
+  );
