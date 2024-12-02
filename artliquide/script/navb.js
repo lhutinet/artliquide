f31d741442c05971beb3f883e317955f1a638eb3
laurent <lhutinet@free.fr>
2024-12-02 12:12:10 +0100

    Delete artliquide directory

--- artliquide/script/navb.js
+++
@@ -1,8 +0,0 @@
-fetch("nav.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("navbar").innerHTML = data;
-  })
-  .catch((error) =>
-    console.error("Erreur lors du chargement de la navigation :", error)
-  );
